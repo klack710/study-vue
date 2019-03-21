11210200b129cc4778233cc89693a352d3224ef5
klack710 <willow710kut@gmail.com>
2019-03-21 23:05:18 +0900

    URLを相対パスに変更

--- app/study-vue/public/main.js
+++ app/study-vue/public/main.js
@@ -3,15 +3,15 @@ var app = new Vue({
   data: {
     link_list: {
       hello: {
-        link_url: "/hello/index.html",
+        link_url: "./hello/index.html",
         link_name: "hello"
       },
       bind_click: {
-        link_url: "/bind_click/index.html",
+        link_url: "./bind_click/index.html",
         link_name: "bind_click"
       },
       data: {
-        link_url: "/data/index.html",
+        link_url: "./data/index.html",
         link_name: "data"
       }
     }
